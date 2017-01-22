a46acabb485aa22bf81ae5c644570b2f9c95882c
desperado_98 <desperado_98@naver.com>
2017-01-22 12:39:32 +0900

    테스트용 QxEffectManager

---
+++ assets/LaputanScript/QxEffectManager.lua
@@ -0,0 +1,19 @@
+
+
+gQxEffectMgr = nil
+
+function createQxEffectManager()
+    gQxEffectMgr = {}
+    
+    gQxEffectMgr._rootGo = 
+
+    function gQxEffectMgr:explosionFragment(fragmentCount, spriteIds, tick)
+        
+    end
+
+    function gQxEffectMgr:shoot(spriteId, startPos, endPos, speed)
+        
+    end
+    
+end
+
